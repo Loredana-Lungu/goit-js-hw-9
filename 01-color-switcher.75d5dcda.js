724617430c35b0dcf014aa72543fbf35158b9355
Loredana-Lungu <158364067+Loredana-Lungu@users.noreply.github.com>
2024-12-06 14:12:12 +0200

    Updates

---
+++ 01-color-switcher.75d5dcda.js
@@ -0,0 +1,2 @@
+let t;document.querySelector("[data-start]").addEventListener("click",(function(){this.disabled=!0,t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){clearInterval(t),document.querySelector("[data-start]").disabled=!1}));
+//# sourceMappingURL=01-color-switcher.75d5dcda.js.map
